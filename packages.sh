96d550a1ca4ba1311dadf87cdc38284d8aa84c22
Romain Goffe <romain.goffe@gmail.com>
2011-01-03 17:23:34 +0100

    add packages

--- packages.sh
+++ packages.sh
@@ -2,23 +2,24 @@
 #Author: Romain Goffe
 #Date: 10/12/2010
 #Description: packages install for ubuntu
+#needs to be run as root
 
-FLAGS= --yes --quiet --ignore-missing #--simulate
+FLAGS="--yes --quiet --ignore-missing" # --simulate"
 
-sudo apt-get update ;
+apt-get update ;
 
 #Dev
-sudo apt-get $FLAGS install awesome build-essential emacs git-core qgit libmagick++-dev perl python perl subversion cmake valgrind kcachegrind qt4-dev-tools libqt4-sql-sqlite libqt4-declarative ;
+apt-get $FLAGS install awesome build-essential emacs git-core qgit libmagick++-dev perl python perl subversion cmake valgrind kcachegrind qt4-dev-tools libqt4-sql-sqlite libqt4-declarative ;
 
 #Multimedia
-sudo apt-get $FLAGS install audacity audacious audacious-plugins easytag gimp gstreamer0.10-ffmpeg gstreamer0.10-fluendo-mp3 gstreamer0.10-plugins-bad imagemagick inkscape mplayer xcfa arista vlc lilypond gimp gimp-plugin-registry ;
+apt-get $FLAGS install audacity audacious audacious-plugins easytag gimp gstreamer0.10-ffmpeg gstreamer0.10-fluendo-mp3 gstreamer0.10-plugins-bad imagemagick inkscape mplayer xcfa arista vlc lilypond gimp gimp-plugin-registry ;
 
 #LaTeX
-sudo apt-get $FLAGS install texlive texlive-lang-french texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra ;
+apt-get $FLAGS install texlive texlive-lang-french texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra texlive-xetex ;
 
 #Tools
-sudo apt-get $FLAGS install gparted aspell-fr gcstar unrar ntfs-3g pdf2svg pdfmod htop ubuntu-restricted-extras ;
+apt-get $FLAGS install filezilla transfig gparted aspell-fr gcstar unrar ntfs-3g pdf2svg pdfmod htop ubuntu-restricted-extras ;
 
-sudo apt-get --yes --quiet upgrade ;
+apt-get --yes --quiet upgrade ;
 
 exit 0
