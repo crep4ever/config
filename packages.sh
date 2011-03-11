bf196a81f177f30dfbfb8260ac7462dbce22038e
crep4ever <romain.goffe@gmail.com>
2011-03-11 17:09:36 +0100

    remove sudo -> run script as root

--- packages.sh
+++ packages.sh
@@ -5,23 +5,23 @@
 
 FLAGS=" --yes --quiet --ignore-missing "#--simulate
 
-sudo apt-get update ;
+apt-get update ;
 
 #Dev
-sudo apt-get install $FLAGS awesome build-essential emacs git-core qgit libmagick++-dev perl python perl subversion cmake valgrind kcachegrind qt4-dev-tools libqt4-sql-sqlite libqt4-declarative;
+apt-get install $FLAGS awesome build-essential emacs git-core qgit libmagick++-dev perl python perl subversion cmake valgrind kcachegrind qt4-dev-tools libqt4-sql-sqlite libqt4-declarative;
 
 #Multimedia
-sudo apt-get install $FLAGS audacity audacious audacious-plugins easytag gimp gstreamer0.10-ffmpeg gstreamer0.10-fluendo-mp3 gstreamer0.10-plugins-bad imagemagick inkscape mplayer xcfa arista vlc lilypond gimp gimp-plugin-registry cheese;
+apt-get install $FLAGS audacity audacious audacious-plugins easytag gimp gstreamer0.10-ffmpeg gstreamer0.10-fluendo-mp3 gstreamer0.10-plugins-bad imagemagick inkscape mplayer xcfa arista vlc lilypond gimp gimp-plugin-registry cheese;
 
 #Internet
-sudo apt-get install $FLAGS filezilla gm-notify ;
+apt-get install $FLAGS filezilla gm-notify ;
 
 #LaTeX
-sudo apt-get install $FLAGS texlive texlive-lang-french texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra bibtex2html;
+apt-get install $FLAGS texlive texlive-lang-french texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra bibtex2html;
 
 #Tools
-sudo apt-get install $FLAGS gparted aspell-fr gcstar unrar ntfs-3g pdf2svg pdfmod htop ubuntu-restricted-extras lame;
+apt-get install $FLAGS gparted aspell-fr gcstar unrar ntfs-3g pdf2svg pdfmod htop ubuntu-restricted-extras lame;
 
-sudo apt-get upgrade --yes --quiet;
+apt-get upgrade --yes --quiet;
 
 exit 0
