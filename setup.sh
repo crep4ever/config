ebbb4600820c0fbfa20f9ca020d67f0549ca78c3
Romain Goffe <romain.goffe@gmail.com>
2011-05-14 19:35:27 +0200

    remove trailing ;

--- setup.sh
+++ setup.sh
@@ -4,26 +4,26 @@
 #Description: setup configuration files in home directory
 #todo el-get + inkscape texttex
 
-cp bashrc $HOME/.bashrc ;
-source $HOME/.bashrc ;
-cp gitconfig $HOME/.gitconfig ;
-cp face $HOME/.face ;
+cp bashrc $HOME/.bashrc
+source $HOME/.bashrc
+cp gitconfig $HOME/.gitconfig
+cp face $HOME/.face
 
 #mplayer
-mkdir -p $HOME/.mplayer/ ;
-cp mplayer $HOME/.mplayer/config ;
+mkdir -p $HOME/.mplayer/
+cp mplayer $HOME/.mplayer/config
 
 #emacs
-cp emacs $HOME/.emacs ;
-mkdir -p $HOME/.config/ ;
-rm -rf $HOME/.config/emacs/ ;
-rm -f $HOME/.config/emacs ;
-cp -r emacs.d/ $HOME/.config/ ;
-mv $HOME/.config/emacs.d/ $HOME/.config/emacs ;
+cp emacs $HOME/.emacs
+mkdir -p $HOME/.config/
+rm -rf $HOME/.config/emacs/
+rm -f $HOME/.config/emacs
+cp -r emacs.d/ $HOME/.config/
+mv $HOME/.config/emacs.d/ $HOME/.config/emacs
 
 #songbook
-mkdir -p $HOME/.local/share/gtksourceview-2.0/language-specs/ ;
-mkdir -p $HOME/.local/share/mime/packages/ ;
-cp songbook/songbook.el $HOME/.config/emacs ;
-cp songbook/songbook.lang  $HOME/.local/share/gtksourceview-2.0/language-specs ;
-cp songbook/songbook.xml  $HOME/.local/share/mime/packages ;
+mkdir -p $HOME/.local/share/gtksourceview-2.0/language-specs/
+mkdir -p $HOME/.local/share/mime/packages/
+cp songbook/songbook.el $HOME/.config/emacs
+cp songbook/songbook.lang  $HOME/.local/share/gtksourceview-2.0/language-specs
+cp songbook/songbook.xml  $HOME/.local/share/mime/packages
